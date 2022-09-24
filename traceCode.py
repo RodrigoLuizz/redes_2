19b87c520b31db70d7835e743ff6e16bc92d4a2e
Rodrigo Luiz <67244458+RodrigoLuizz@users.noreply.github.com>
2022-09-24 08:09:17 -0300

    Add files via upload

---
+++ traceCode.py
@@ -0,0 +1,62 @@
+﻿#!/usr/bin/env python3
+
+import socket
+import time
+import dns.resolver as res
+import re
+
+port = 33434
+ttl = 0
+#alvo = '200.133.2.18'
+alvo = 'poli.br'
+x = re.search("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", alvo) 
+
+if x:
+  print("IP Address")
+else:
+  print("Domain Name: ", alvo)
+  rA = res.resolve(alvo, 'A')
+  for ipval in rA:
+    x = ipval.to_text()
+print('Endereco IP do alvo: ', x)
+#alvo = input('Digite o endereco do alvo: ')
+#alvo = input('Digite o dominio do alvo: ')
+# ------------------------------------------------------------------
+while True:
+    ttl += 1
+    print('\nTTL: ', ttl)
+    socket_recv = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    socket_recv.settimeout(10)
+    socket_recv.bind(('', 0))
+    
+    socket_sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    socket_sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+    
+    #socket_sender.sendto(b'', (alvo, port))
+    socket_sender.sendto(b'', (x, port))
+    
+    try:
+        rcv = socket_recv.recvfrom(1024)
+        data = rcv[0]
+        addr = rcv[1]
+        print('Data: ', data)
+        
+        #if ord(rcv[9]) == '1':
+        print('Protocolo: ', data[9])
+        print('TipoIcmp', data[20]) #deve ser 11
+        print('CodIcmp', data[21]) #deve ser 0
+        print('Maquina remota: ', addr[0])
+        
+        # print('\n', addr[0], ttl)
+        # if addr[0] == x or ttl > 30:
+            # break
+        
+    except socket.timeout:
+        print('TIME OUT!!!')
+
+    #print('\n', addr[0], ttl)
+    if addr[0] == x or ttl > 30:
+        break
+    
+socket_recv.close()
+socket_sender.close()
